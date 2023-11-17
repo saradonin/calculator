b509ed9a1fda68f1b4fb4803fdc701927fb4aca4
saradonin <m.jemieljanczuk@gmail.com>
2023-11-17 20:11:15 +0100

    error handling added

--- main.py
+++ main.py
@@ -9,24 +9,27 @@ def calculate_volume():
     if request.method == "GET":
         return render_template("calculator_form.html")
     else:
-        initial_volume = float(request.form.get("initial_volume",0))
-        initial_weight = float(request.form.get("initial_weight",0))
-        current_weight = float(request.form.get("current_weight",0))
-        density = float(request.form.get("density",0.84))
-
-        current_volume = round(Bottle.calculate_current_volume(
-            initial_volume,
-            initial_weight,
-            current_weight,
-            density
-        ), 2)
-
-        return render_template("calculator_form.html", 
-                               current_volume=str(current_volume)+" ml", 
-                               initial_volume=initial_volume, 
-                               initial_weight=initial_weight,
-                               current_weight=current_weight,
-                               density=density)
+        try:
+            initial_volume = float(request.form.get("initial_volume",0))
+            initial_weight = float(request.form.get("initial_weight",0))
+            current_weight = float(request.form.get("current_weight",0))
+            density = float(request.form.get("density",0.84))
+            current_volume = round(Bottle.calculate_current_volume(
+                initial_volume,
+                initial_weight,
+                current_weight,
+                density
+            ), 2)
+            
+            return render_template("calculator_form.html", 
+                                current_volume=str(current_volume)+" ml", 
+                                initial_volume=initial_volume, 
+                                initial_weight=initial_weight,
+                                current_weight=current_weight,
+                                density=density)
+        except ValueError:
+            message = "Wypełnij poprawnie wszystkie pola"
+            return render_template("calculator_form.html", message=message)
 
 
 if __name__ == "__main__":
