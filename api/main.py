ea053e78559ea420102850bc2b00e50745cfb54f
saradonin <m.jemieljanczuk@gmail.com>
2023-11-17 19:20:02 +0100

    app run

--- api/main.py
+++ api/main.py
@@ -30,6 +30,4 @@ def calculate_volume():
 
 
 if __name__ == "__main__":
-    calculate_volume()
-# if __name__ == "__main__":
-#     app.run(debug=True, port=5001)
+    app.run(debug=True, port=5001)
