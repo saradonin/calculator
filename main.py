16db9b325ff0d7a7d90bb457da6fd6e0c6253b3c
saradonin <m.jemieljanczuk@gmail.com>
2023-11-18 17:20:57 +0100

    app mechanic simplified

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from models import Bottle
+
 
 app = Flask(__name__)
 
@@ -14,23 +14,20 @@ def calculate_volume():
             initial_weight = float(request.form.get("initial_weight"))
             current_weight = float(request.form.get("current_weight"))
             density = float(request.form.get("density"))
-            current_volume = round(Bottle.calculate_current_volume(
-                initial_volume,
-                initial_weight,
-                current_weight,
-                density
-            ), 2)
-            
-            return render_template("calculator_form.html", 
-                                current_volume=str(current_volume)+" ml", 
-                                initial_volume=initial_volume, 
-                                initial_weight=initial_weight,
-                                current_weight=current_weight,
-                                density=density)
+
+            current_volume = round(
+                initial_volume - ((initial_weight - current_weight) / density), 2)
+
+            return render_template("calculator_form.html",
+                                   current_volume=current_volume,
+                                   initial_volume=initial_volume,
+                                   initial_weight=initial_weight,
+                                   current_weight=current_weight,
+                                   density=density)
         except ValueError:
             message = "Please fill in all fields correctly."
             return render_template("calculator_form.html", message=message)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5000)
